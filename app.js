ff1d83affc85dc3de02333d7677d1c3611503c9e
rayGreycloud <rayGreycloud@gmail.com>
2018-03-07 13:39:00 -0800

    ended
    Add delete /files/:filename route to delete file from db

--- app.js
+++ app.js
@@ -126,5 +126,17 @@ app.get('/image/:filename', (req, res) => {
   });
 });
 
+// @route DELETE /files/:id
+// @desc Delete file
+app.delete('/files/:id', (req, res) => {
+  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+    
+    res.redirect('/');
+  });
+});
+
 // Start server
 app.listen(port, () => console.log(`Server started and listening on port ${port}`));
